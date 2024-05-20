892a3618e13481db1b3c8c8bb60534bd9a2420fc
dhruv kushwaha <dhruvkushwaha1234@gmail.com>
2024-05-20 21:34:20 +0530

    this is first commit

---
+++ script.js
@@ -0,0 +1,160 @@
+// let x = 5;
+// let y = 2;
+// let z = x + y;
+
+// document.getElementById("demo1").innerHTML = "The sum of x + y is: " + z;
+
+// let a = 3;
+// let x = (100 + 50) * a;
+// document.getElementById("demo").innerHTML = x;
+// addication 
+// let x = 10;
+// x += 5;
+// document.getElementById("demo2").innerHTML = "Value of x is: " + x;
+
+// funcation
+
+// function myFunction(p1, p2) {
+//     return p1 * p2;
+//     // return p1 + p2;
+//     // return p1 - p2;
+//     // return p1 % p2;
+//     // return p1 / p2;
+//     // return  p1 = p2;
+//    }
+  
+//   let result = myFunction(4, 4);
+//   document.getElementById("demo").innerHTML = result;
+// // addication 
+//   function myFunction(p1, p2) {
+  
+//     return p1 + p2;
+//     // return p1 - p2;
+//     // return p1 % p2;
+//     // return p1 / p2;
+//     // return  p1 = p2;
+//    }
+  
+//   let result1 = myFunction(4, 4);
+//   document.getElementById("demo").innerHTML = result;
+
+
+//   const person = {
+//     firstName: "dhruv ",
+//     lastName: "kushwaha",
+//     age: 21,
+//     eyeColor: "black"
+//   };
+//   document.getElementById("demo1").innerHTML =
+//   person.firstName + " is " + person.age + " years old.";
+
+//   // evant
+  
+
+//   function displayDate() {
+//     document.getElementById("demo").innerHTML = Date();
+//   }
+
+//   //
+//   function addication(){
+//     document.getElementById("demo").innerHTML= the;
+//   }
+// //
+//   // string mathod
+//   let text = "ram, shayma, pankaj";
+//   let part = text.slice(7)
+//   document.getElementById("demo1").innerHTML = part;
+
+
+
+// let str = "Apple, Kiwi";
+// document.getElementById("demo1").innerHTML = str.substring(7,13);
+
+
+
+
+
+
+
+// let str1 = " dhruv, kumar, kushwaha";
+//   document.getElementById ("demo2"). innerHTML= str.substring(5,10);
+
+
+  // replace method
+
+
+//   function myFunction() {
+//     let text = document.getElementById("demo").innerHTML; 
+//     document.getElementById("demo").innerHTML = text.replace("Microsoft","W3Schools");
+
+//   }
+
+
+//   function newfunction()
+//   {
+//    let text = document.getElementById("demo"). innerHTML;
+//    document.getElementById("demo").innerHTML = text.split("Microsoft");
+
+//   }
+
+
+  // function myFunction()
+  // {
+  //   let text = document.getElementById("demo").innerHTML;
+  //   document.getElementById("demo").innerHTML= text.replace("replace","replacing ");
+
+  // }
+  //CONCAT
+
+//   let text1 = "DHRUV KUMAR";
+// let text2 = "KUSHWAHA";
+// let text3 = text1.concat(" like this any name",text2);
+// document.getElementById("demo").innerHTML = text3;
+
+// let text1 = "dhruv";
+// let text2 = "kushwaha";
+// let result = text1.concat(text2);
+
+// document.getElementById("demo").innerHTML = result;
+
+
+// let text1 = "DHRUV KUMAR KUSHWAHA";
+// let text2 = text1.trim();
+
+// document.getElementById("demo").innerHTML =
+// "Length text1 = " + text1.length + "<br>Length text2 = " + text2.length;
+
+// endwith()
+
+// let text = "Hello World";
+// let result = text.endsWith("");
+
+// document.getElementById("demo").innerHTML = result;
+// string
+// let text = "dhruv kumar kushwaha";
+// document.getElementById("demo").innerHTML = text.length;
+// document.getElementById("demo").innerHTML = text.charCodeAt(0);
+//The at() Method
+const name = "hello this is  java script";
+let letter = name.at(8);
+
+//document.getElementById("demo").innerHTML = letter;
+
+
+// repeat method
+
+// let text = "java script ";
+// let result = text.repeat(2);
+
+// document.getElementById("demo").innerHTML = result;
+
+//
+let text = "5";
+text = text.padEnd(4,"0");
+
+document.getElementById("demo").innerHTML = text;
+
+
+
+
+
